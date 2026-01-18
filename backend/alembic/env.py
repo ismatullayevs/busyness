7ed9161618e0337ffb3d4554c60f44117af96f1c
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2026-01-18 17:58:32 +0500

    Fix postgresql url issue

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -14,7 +14,7 @@ from app.models.task import Task, TaskLog  # noqa: F401
 config = context.config
 
 # Set the database URL from settings
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.sqlalchemy_database_url)
 
 # Interpret the config file for Python logging.
 if config.config_file_name is not None:
